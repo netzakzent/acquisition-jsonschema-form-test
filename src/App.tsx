323229128636064a8437809dae6404027919dd9e
Walter Leinert <walter.leinert@aracom.de>
2018-04-20 11:32:18 +0200

    added formData

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,34 @@ import * as React from 'react';
 import Form from "react-jsonschema-form";
 import './App.css';
 
-const schema: JSONSchema6 = {  
+const schema: JSONSchema6 = {
   properties: {
-    done: {type: "boolean", title: "Done?", default: false},
-    title: {type: "string", title: "Title", default: "A new task"},
+    done: { type: "boolean", title: "Done?", default: false },
+    title: { type: "string", title: "Title", default: "A new task" },
   },
   required: ["title"],
   title: "Todo",
   type: "object",
 };
 
+
+const formData = {
+  done: true,
+  title: "First task",
+};
+
+
 const log = (type: any) => console.log.bind(console, type);
 
 class App extends React.Component {
   public render() {
     return (
-      <Form schema={schema}
-      onChange={log("changed")}
-      onSubmit={log("submitted")}
-      onError={log("errors")} />
+      <Form 
+        schema={schema}
+        formData={formData}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")} />
     );
   }
 }
