742f08cef7315563b7abaca63bbeff39fd5daaed
Walter Leinert <walter.leinert@aracom.de>
2018-04-24 14:57:05 +0200

    refactorings, added deepmerge

--- src/akquise/AkquiseForm.tsx
+++ src/akquise/AkquiseForm.tsx
@@ -3,12 +3,21 @@
 // tslint:disable:max-classes-per-file
 
 
+
+import { JSONSchema6 } from 'json-schema';
 import * as React from 'react';
 import Form from "react-jsonschema-form";
 
+// tslint:disable-next-line:no-var-requires
+const merge = require('deepmerge').default;
 import * as data from './recipient/formData.json';
+import * as schemaDiff from './recipient/schema-diff.json';
 import * as schema from './recipient/schema.json';
 import * as uiSchema from './recipient/uiSchema.json';
+
+
+
+const schemaMerged = merge(schema, schemaDiff) as JSONSchema6;
 
 
 const log = (type: string) => console.log.bind(console, type);
@@ -21,11 +30,12 @@ export default class AkquiseForm extends React.Component {
     return (
       <Form
         formData={data}
-        schema={schema}
+        schema={schemaMerged}
         uiSchema={uiSchema}
         onChange={log("changed")}
         onSubmit={onSubmit}
-        onError={onError} >
+        onError={onError} liveValidate={true} >
+
 
         <div>
           <button type="submit">Submit</button>
